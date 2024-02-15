aec8679078d910d5d9cb823f5b8e8d03323dcd6b
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-02-15 18:30:20 +0000

    it works

--- SnakesAndLaddersExtra/obj/Debug/net8.0/SnakesAndLaddersExtra.AssemblyInfo.cs
+++ SnakesAndLaddersExtra/obj/Debug/net8.0/SnakesAndLaddersExtra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ec90034f7fb4bc9416af9f1560a87d35d9407f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b8a6529cfb631132c888d7c109c0524088704c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
