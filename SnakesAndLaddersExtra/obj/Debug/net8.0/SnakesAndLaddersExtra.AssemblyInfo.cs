52f7dc886655825c514cce8ff272b6392e18d563
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-02-15 17:45:15 +0000

    edit 3

--- SnakesAndLaddersExtra/obj/Debug/net8.0/SnakesAndLaddersExtra.AssemblyInfo.cs
+++ SnakesAndLaddersExtra/obj/Debug/net8.0/SnakesAndLaddersExtra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2edbca6100fb7da9c3bef8f3e2cc80b76f08f5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce091d684167671a61d48e3dc564e2ceaa29e86")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
