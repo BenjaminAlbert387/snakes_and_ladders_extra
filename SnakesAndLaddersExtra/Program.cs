f81ef8cb67c5a9ab2f6bed6174d9b506e9cebc39
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-02-15 18:18:52 +0000

    added p1 name

--- SnakesAndLaddersExtra/Program.cs
+++ SnakesAndLaddersExtra/Program.cs
@@ -4,7 +4,30 @@
     {
         Console.WriteLine("Welcome to Snakes and Ladders: Extra!");
         Console.WriteLine("Press the Ready button when you are ready to play.");
+
+        string playerOneName = GetPlayerOneName();
     }
+
+    public static string GetPlayerOneName()
+    {
+        Console.WriteLine("Select a letter for Player 1: A, B, C or D.")
+
+        string[] validPlayerOneName = new [] {"A", "B", "C", "D"};
+        string playerOneName;
+
+        do
+        {
+            playerOneName = Console.ReadLine().ToUpper();
+
+            if (!validPlayerOneName.Contains(playerOneName));
+            {
+                Console.WriteLine("That's not valid. Try again.");
+            }
+        }
+        while (!validPlayerOneName.Contains(playerOneName));
+        return playerOneName
+    }
+
 
 
 }
