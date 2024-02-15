c034e41fdb9238852a4e5a2a2eef9fcbf1a6996b
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-02-15 17:46:26 +0000

    edit 4

--- SnakesAndLaddersExtra/Program.cs
+++ SnakesAndLaddersExtra/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Welcome to Snakes and Ladders: Extra!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Snakes and Ladders: Extra!");
+    }
+}
