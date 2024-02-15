593ba2dbb3ffa61602f1e775ebe99d376290599c
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-02-15 18:02:18 +0000

    edit 9

--- SnakesAndLaddersExtra/Program.cs
+++ SnakesAndLaddersExtra/Program.cs
@@ -3,7 +3,8 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Snakes and Ladders: Extra!");
+        Console.WriteLine("Press the Ready button when you are ready to play.");
     }
 
-    
+
 }
