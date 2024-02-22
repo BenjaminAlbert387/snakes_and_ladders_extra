243c5b24c2a8bfd744f13d4e8a5851b7a84f25d1
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-02-22 17:56:29 +0000

    works?

--- SnakesAndLaddersExtra/obj/Debug/net8.0/SnakesAndLaddersExtra.AssemblyInfo.cs
+++ SnakesAndLaddersExtra/obj/Debug/net8.0/SnakesAndLaddersExtra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dedca0752881455002ffe0f2de1bdeb47ec4fc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc3da4e134f8a714f603db127d4c56a06fdd873")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnakesAndLaddersExtra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
